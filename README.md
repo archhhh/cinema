0bbc06bef00bbccb161c38a9fb9193a58a37c237
Artyom Chen <artychen13@gmail.com>
2019-08-02 04:50:14 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# cinema
+# Cinema
+
+TBD
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./javascript/components/App";
+import "./styles/style.scss";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("app")
+);
